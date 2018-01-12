5461bea0c53a5d697c46a7fb6667ff1fac3dbe38
vjq姜强 <qiangjiang@ctrip.com>
2018-01-12 16:37:56 +0800

    first time

---
+++ src/main/java/Application.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+/**
+ * Created by qiangjiang on 2018/1/12.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("test");
+        /*
+        System.out.println("please press any key to exit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(1);
+        */
+    }
+}
